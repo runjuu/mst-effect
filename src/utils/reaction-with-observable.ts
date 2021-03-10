d819b28283df254e0be0e84041649d2b4826f751
Runjuu <hi@runjuu.com>
2021-03-10 11:55:31 +0800

    feat(utils): update `reaction$` signature

--- src/utils/reaction-with-observable.ts
+++ src/utils/reaction-with-observable.ts
@@ -3,10 +3,24 @@ import { Observable } from 'rxjs'
 
 export function reaction$<T>(
   expression: (r: IReactionPublic) => T,
+  opts: Omit<IReactionOptions, 'fireImmediately'> & { fireImmediately: true },
+): Observable<{ current: T; prev: T | undefined; r: IReactionPublic }>
+
+export function reaction$<T>(
+  expression: (r: IReactionPublic) => T,
   opts?: IReactionOptions,
-): Observable<T> {
+): Observable<{ current: T; prev: T; r: IReactionPublic }>
+
+export function reaction$<T>(
+  expression: (r: IReactionPublic) => T,
+  opts?: IReactionOptions,
+): Observable<{ current: T; prev: T; r: IReactionPublic }> {
   return new Observable((observer) => {
-    const dispose = reaction(expression, (value) => observer.next(value), opts)
+    const dispose = reaction(
+      expression,
+      (current, prev, r) => observer.next({ current, prev, r }),
+      opts,
+    )
 
     return () => dispose()
   })

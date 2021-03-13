88793403284ff27ada1681829b74c56d28919f48
Runjuu <hi@runjuu.com>
2021-03-13 13:27:18 +0800

    feat: introduce `signal`

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './types'
 export * from './effect'
+export * from './signal'
 export * from './reaction-with-observable'
---
+++ src/signal/index.ts
@@ -0,0 +1,19 @@
+import { addDisposer } from 'mobx-state-tree'
+import { Observable, Subject } from 'rxjs'
+
+import { AnyInstance, PayloadFunc } from '../types'
+
+export type SignalFactory<P, R> = (payload$: Observable<P>) => Observable<R>
+
+export type SignalResult<P, R> = [PayloadFunc<P, void>, Observable<R>]
+
+export function signal<P>(self: AnyInstance): SignalResult<P, P>
+export function signal<P, R>(self: AnyInstance, fn: SignalFactory<P, R>): SignalResult<P, R>
+export function signal<P, R>(self: AnyInstance, fn?: SignalFactory<P, R>): SignalResult<P, R> {
+  const payloadSource = new Subject<P>()
+  const signal$ = fn?.(payloadSource.asObservable()) ?? payloadSource.asObservable()
+
+  addDisposer(self, () => payloadSource.complete())
+
+  return [(payload) => payloadSource.next(payload), signal$] as SignalResult<P, R>
+}
